f830e2ceeb87a57e6725d42a8a43ddbee8df52b0
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-08-01 13:44:26 -0400

    blog controller building

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,23 @@
 class BlogsController < ApplicationController
-
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    
     # GET
 
     # POST 
+    def create
+        user = User.find_by(id: session[:user_id])
+        blog = user.blogs.create!(blog_params)
+        render json: blog, status: :created
+    end
 
     # PATCH
 
     # DELETE
 
-    # PRIVATE
+    private
+
+    def blog_params
+        params.permit(:id, :title, :content, :published_on)
+    end
 
 end
