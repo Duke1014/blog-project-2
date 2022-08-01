850836f5f90c4db21589d472d9db1d3f2632d1fe
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-08-01 14:15:20 -0400

    comments controller built

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    # GET
+    
+    # POST 
+    def create
+        user = User.find_by(id: session[:user_id])
+        comment = user.comments.create!(comment_params)
+        render json: user, status: :created
+    end
+
+    # PATCH
+
+    # DELETE
+
+    private
+
+    def comment_params
+        params.permit(:id, :content, :published_on)
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
