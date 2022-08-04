2d6438af02e5d2312747308958065a9439fc288f
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-08-03 22:27:17 -0400

    seed initialized

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+100.times do
+    username = Faker::Internet.username
+    password = Faker::Internet.password
+    user = User.create!(username: username, password: password)
+    10.times do 
+        title = Faker::Lorem.sentence
+        content = Faker::Lorem.paragraph
+        date = Faker::Date.between(from: '2014-09-23', to: Date.today)
+        blog = user.blogs.create!(title: title, content: content, published_on: date)
+        4.times do
+            comment_content = Faker::Lorem.paragraph
+            comment_date = Faker::Date.between(from: '2014-09-23', to: Date.today)
+            comment = user.comments.create!(content: comment_content, published_on: comment_date, blog_id: blog.id)
+        end
+    end
+end
